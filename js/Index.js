2c4b58425683b79eae676906530f500ba3e1aa78
samernassar12 <51993942+samernassar12@users.noreply.github.com>
2019-06-19 13:06:31 +0300

    Add files via upload

---
+++ js/Index.js
@@ -0,0 +1,16 @@
+﻿
+    if ($(window).width() > 768) {
+                var i = 0;
+                $('.dropdown').hover(function () {
+                    if (i == 0) {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(500);
+    i = 1;
+}
+                    else {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(400);
+
+    i = 0;
+}
+});
+}
+
